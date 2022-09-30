5fcfd6be7c9b6a707c2da43dfd1ab548f4582e87
Tajhoba Williams <89955965+Tajhoba@users.noreply.github.com>
2022-09-29 18:02:36 -0700

    added float and Chavi Calculator by Tajhoba Williams

--- src/demo/src/main/java/com/democalculator/App.java
+++ src/demo/src/main/java/com/democalculator/App.java
@@ -12,3 +12,9 @@ public class Calculator
     Scanner number = new Scanner(System.in);
     do
     {
+        System.out.println("-------------------------");
+        System.out.println("Chavi Calculator by Tajhoba Williams");
+        System.out.println("-------------------------");
+        System.out.println("Commend : \n1.Add\n2.Subtract\n3.Divide\n4.Multiply\n5.Clear\n6.Quit");
+        System.out.print("Enter your command : ");
+        commend=number.next().charAt(0);
